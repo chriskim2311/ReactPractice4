7590876eff6704cc516ab3c69ee98db004cc7c0c
chriskim2311 <chriskim2311@gmail.com>
2019-04-05 20:44:26 -0700

    Completed app

---
+++ blog/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PostList from './components/PostList'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+      <PostList/>
+
+      </div>
+    )
+  }
+}
+
+export default App;
